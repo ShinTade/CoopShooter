b0bbae1925559232439eeba7da6c28e03000d8b8
Tomasz Figas <tomasz.figas@gmail.com>
2018-10-16 11:50:49 +0200

    47. Third Person Camera Part 1

--- Source/CoopShooter/Private/SCharacter.cpp
+++ Source/CoopShooter/Private/SCharacter.cpp
@@ -1,13 +1,16 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SCharacter.h"
-
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 ASCharacter::ASCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComp->bUsePawnControlRotation = true;
 
 }
 
