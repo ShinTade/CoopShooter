d4d0ad422ae14f49b96890dbe8113fbefcd5e948
Tomasz Figas <tomasz.figas@gmail.com>
2019-04-23 16:03:00 +0200

    Project upgrade to 4.19

--- Source/CoopShooter/CoopShooter.Build.cs
+++ Source/CoopShooter/CoopShooter.Build.cs
@@ -8,7 +8,7 @@ public class CoopShooter : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "InstanceTool" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
